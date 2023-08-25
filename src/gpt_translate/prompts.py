ad582c4bfb142678ad213c6e873464755a9ef41c
shex1627 <alistar.huang@gmail.com>
2023-08-25 00:57:09 -0700

    add question to answer

--- src/gpt_translate/prompts.py
+++ src/gpt_translate/prompts.py
@@ -55,3 +55,40 @@ def translate_title_prompt(text: str, token_limit: int= 1500):
     #num_tokens = num_tokens_from_messages(messages)
     #print(f"num_tokens {num_tokens}")
     return messages
+
+def key_context_prompt(question: str):
+    """
+    generate messages
+    """
+    messages = [
+      {"role": "system", "content": "You are a QA bot that finds relevant information to answer the given question."},
+      {"role": "user", "content": f'extract key entities from below question "{question}", return only the list of key entities as plain text"'}
+    ]
+    #num_tokens = num_tokens_from_messages(messages)
+    #print(f"num_tokens {num_tokens}")
+    return messages
+
+def single_article_prompt(question: str, article: str, word_limit: int= 2000, language: str = "Chinese"):
+    """
+    generate messages
+    """
+    messages = [
+      {"role": "system", "content": "You are a QA bot that finds relevant information to answer the given question, based on the following article, if you can not give a definite answer, still provide relevant information}"},
+      {"role": "user", "content": f'In less than {word_limit} words, answering in {language} language, return important context to answer the following question: {question}, based on the following article, if you can not give a definite answer, still provide relevant information, aritcle: {article}"'}
+    ]
+    #num_tokens = num_tokens_from_messages(messages)
+    #print(f"num_tokens {num_tokens}")
+    return messages
+
+
+def aggregate_article_prompt(question: str, article: str, word_limit: int= 2000, language: str = "Chinese"):
+    """
+    generate messages
+    """
+    messages = [
+      {"role": "system", "content": "You are a QA bot that tries to answer the given question based on analyzing the given text, if you can not give a definite answer, express that and summarize the information from given context."},
+      {"role": "user", "content": f'Below are answers to question {question} based on different articles, oragnize all the answers into a coherent essay in less than {word_limit} words, answering in {language} language". Context: {article}"'}
+    ]
+    #num_tokens = num_tokens_from_messages(messages)
+    #print(f"num_tokens {num_tokens}")
+    return messages
---
+++ src/streamlit_app/question.py
@@ -0,0 +1,179 @@
+
+import openai
+import os
+import json
+import streamlit as st
+import pandas as pd
+import os 
+from urllib.request import urlopen
+from PIL import Image
+import random
+import logging 
+from st_aggrid import AgGrid
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+from st_aggrid import GridUpdateMode, DataReturnMode
+from gpt_translate.utils import add_newlines
+from gpt_translate.articles.JsonArticleManager import JsonArticleManager
+from gpt_translate.article_to_answer import parallel_send_chatcomplete_api
+from gpt_translate.prompts import key_context_prompt, single_article_prompt, aggregate_article_prompt
+from gpt_translate.article_to_translation import text_split, send_chatcomplete_api, combine_openapi_response
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+TOP_K_ARTICLES = 5
+
+SINGLE_ARTICLE_SUMMARY_LENGTH = 2048
+SUNMMARY_LENGTH = 4000
+OUTPUT_FILE_PATH = os.environ.get("OUTPUT_FILE_PATH", "./output.json")
+
+completion_config = {
+    'model': "gpt-3.5-turbo-16k",
+    'temperature': 0
+}
+
+gpt4_config = {
+    'model': "gpt-4",
+    'temperature': 0,
+}
+
+@st.cache_data
+def get_article_manager():
+   article_file_path = os.environ.get('ARTICLE_DB_PATH', "C:\\Users\\alistar\\Desktop\\ds\\blogger_translate\\articles_embedding.json")
+   print(f"article_file_path {article_file_path}")
+   article_manager = JsonArticleManager(article_file_path)
+   return article_manager
+
+@st.cache_data
+def get_article_summary(all_context_text: str, question: str, language: str = "Chinese"):
+    text_trunks = text_split(all_context_text, max_length=SINGLE_ARTICLE_SUMMARY_LENGTH)
+    trunk_messages = [single_article_prompt(question, text_trunk, word_limit=SINGLE_ARTICLE_SUMMARY_LENGTH, 
+                                            language=language) for text_trunk in text_trunks]
+    responses = parallel_send_chatcomplete_api(trunk_messages, gpt4_config)
+    trunk_processed = [combine_openapi_response(response)[0] for response in responses]
+    return trunk_processed
+
+@st.cache_data
+def agggregate_article_summary(article_summaries: str, question: str, language: str = "Chinese"):
+    aggregate_answers_msg = aggregate_article_prompt(question, article_summaries, 
+                                                     word_limit = SINGLE_ARTICLE_SUMMARY_LENGTH, 
+                                                     language=language)
+    response = openai.ChatCompletion.create(**gpt4_config, messages=aggregate_answers_msg)
+    return response
+    sentences = []
+    sentences.append("=====================================\n")
+    sentences(f"question: {question}\n")
+    sentences(f"final summarys\n")
+    sentences("=====================================\n")
+    for sentence in response.choices[0]['text'].split(st.session_state.period):
+        if sentence:
+            sentences.append(sentence)
+            sentences.append("\n")
+    return sentences
+
+# set up page icon
+#page_icon = Image.open(urlopen("https://i.imgur.com/z7ZGWvZ.jpg"))
+
+# Streamlit App
+st.set_page_config(page_title="Article Search", 
+                   #page_icon=page_icon, 
+                   layout="wide")
+
+# hide menu
+if os.environ.get('HIDE_MENU', 'true') == 'true':
+        st.markdown("""
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """, unsafe_allow_html=True)
+        
+st.title("Answer Questions from Articles")
+st.write("This website uses GPT-3 to answer questions based on the top 5 relevant articles from a wechat blog. The blog covers a wide range of common topics in life.")
+
+article_manager = get_article_manager()
+query_params = st.experimental_get_query_params()
+language = st.radio("Language:", ["English", "Chinese"], index=0)
+is_random = st.radio("Article Randomness", ["Default", "Random"], index=0)
+st.write("Note: Random means randomly choose 5 out of top 20 relevant articles")
+
+# save random to session state
+st.session_state.is_random = is_random
+
+# save language to session state
+st.session_state.language = language
+if st.session_state.language == "English":
+    st.session_state.period = "."
+    show_cols = ['english_title', 'title']
+elif st.session_state.language == 'Chinese':
+    st.session_state.period = "。"
+    show_cols = ['title', 'english_title']
+
+if 'selected_article' not in st.session_state:
+    st.session_state.selected_article = None
+
+st.header("Ask A Question")
+question = st.text_input("question prompt", max_chars=30)
+
+if st.button("Ask") and question:
+    key_context_resp = openai.ChatCompletion.create(**completion_config, messages=key_context_prompt(question))
+    key_context = key_context_resp.choices[0]['message']['content'].replace("作者,", "")
+    relevant_articles = article_manager.search_by_embedding(key_context) 
+    if st.session_state.is_random == "Default":
+        seed = "None"
+        st.session_state.seed = seed
+        results = relevant_articles[:TOP_K_ARTICLES]
+    else:
+        seed = random.randint(0, 10000)
+        st.session_state.seed = seed
+        results = relevant_articles.sample(n=TOP_K_ARTICLES, random_state=seed)
+    results['article_url'] = results['id'].apply(lambda x: f"https://memory.ftdalpha.com?article_id={x}")
+    results = results.reset_index(drop=True)
+    st.session_state.results = results
+
+if st.session_state.get("results", pd.DataFrame()).shape[0] > 0:
+    # Generate column definitions dynamically
+    # make streamtlit show each row in dataframe st.session_state.results
+    st.header("Relevant Articles")
+    st.write(st.session_state.results[['article_url']+show_cols])
+
+    # load output file 
+    try:
+        with open(OUTPUT_FILE_PATH, "r") as f:
+            output = json.load(f)
+    except:
+        output = {}
+    all_context_text = " ".join(st.session_state.results['text'])
+    trunk_processed = get_article_summary(all_context_text, question, language=st.session_state.language)
+    article_summaries = "\n".join(trunk_processed)
+    article_summary_sentences = []
+    for trunk in trunk_processed:
+        for sentence in trunk.split(st.session_state.period):
+            if sentence:
+                article_summary_sentences.append(sentence)
+                article_summary_sentences.append("\n")
+        article_summary_sentences.append("#####################################\n")
+    st.header("Article Summaries")
+    for trunk in trunk_processed:
+        st.write(trunk)
+        st.write("#####################################\n")
+
+    overall_summary_resp = agggregate_article_summary(article_summaries, question, language=st.session_state.language)
+    print(overall_summary_resp)
+    overall_summary_sentences = []
+    for sentence in overall_summary_resp.choices[0]['message']['content'].split(st.session_state.period):
+        if sentence:
+            overall_summary_sentences.append(sentence)
+            overall_summary_sentences.append("\n")
+
+    st.header("Overall Summary")
+    st.write("".join(overall_summary_sentences))
+    
+    # save current output to another file 
+    with open(OUTPUT_FILE_PATH, "w") as f:
+        output["-".join([question, st.session_state.language, str(st.session_state.seed)])] = {"article_summary_sentences": trunk_processed, 
+                            "overall_summary_sentences": overall_summary_sentences}
+        json.dump(output, f, indent=4)
+    
